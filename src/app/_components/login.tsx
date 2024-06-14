41382f22268f0c471988083c005884af399c11b7
Sergei Ustinov <sergey.ustinov@sensand.com>
2024-06-14 19:15:47 +1000

    [wip] add a tooltip

--- src/app/_components/login.tsx
+++ src/app/_components/login.tsx
@@ -1,16 +1,31 @@
 import { Button } from "@/components/ui/button";
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "@/components/ui/tooltip";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Login() {
   return (
     <>
-      <SignedOut>
-        <SignInButton>
-          <Button asChild className="cursor-pointer" variant="outline">
-            <span className="text-xl">👤</span>
-          </Button>
-        </SignInButton>
-      </SignedOut>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <SignedOut>
+              <SignInButton>
+                <Button asChild className="cursor-pointer" variant="outline">
+                  <span className="text-xl">👤</span>
+                </Button>
+              </SignInButton>
+            </SignedOut>
+          </TooltipTrigger>
+          <TooltipContent>
+            <span className="text-md">✌️ Click to login</span>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
 
       <SignedIn>
         <UserButton />
