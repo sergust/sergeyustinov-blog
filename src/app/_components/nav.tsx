e085857e98fe5d31c1dcb0f7d13d52ed18d2d354
Sergei Ustinov <sergey.ustinov@sensand.com>
2024-06-15 20:24:03 +1000

    [wip] add mobile burger menu for header

--- src/app/_components/nav.tsx
+++ src/app/_components/nav.tsx
@@ -1,17 +1,73 @@
+"use client";
+
 import { ModeToggle } from "@/components/theme-mode-toggle";
 import Login from "./login";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useUser } from "@clerk/clerk-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { SignInButton } from "@clerk/nextjs";
 
 export default function Nav() {
+  const { user } = useUser();
   return (
     <nav className="my-2 flex items-center gap-4 py-4">
-      <a href="/blog" className="ml-4">
-        Blog
-      </a>
-      <a href="/about" className="ml-4">
-        About
-      </a>
-      <Login />
-      <ModeToggle />
+      <div className="hidden items-center gap-4 py-4 md:flex">
+        <Search />
+        <ModeToggle />
+        <Login />
+      </div>
+      <div className="flex md:hidden">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">
+              <span className="text-xl">🍔</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            {user ? (
+              <DropdownMenuLabel>
+                <div className="flex items-center gap-2">
+                  <span>{user?.fullName}</span>
+                  <Login />
+                </div>
+              </DropdownMenuLabel>
+            ) : (
+              <DropdownMenuItem>
+                <SignInButton></SignInButton>
+              </DropdownMenuItem>
+            )}
+
+            <DropdownMenuSeparator />
+
+            <DropdownMenuLabel>
+              <div className="flex justify-between">
+                <Search />
+                <ModeToggle />
+              </div>
+            </DropdownMenuLabel>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </nav>
+  );
+}
+
+function Search() {
+  return (
+    <Button variant="outline" className="text-xl">
+      🔍
+    </Button>
   );
 }
