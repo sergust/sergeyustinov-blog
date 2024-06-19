ae3a35001cf6ecc10e5785988465a77dbe4c3cc6
Sergei Ustinov <sergey.ustinov@sensand.com>
2024-06-19 16:18:09 +1000

    [wip] code component

--- src/components/admin/editor.tsx
+++ src/components/admin/editor.tsx
@@ -1,7 +1,13 @@
 "use client";
 import { useCurrentEditor, EditorProvider } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import { BoldIcon, ItalicIcon, UnderlineIcon } from "lucide-react";
+import {
+  BoldIcon,
+  CodeIcon,
+  ItalicIcon,
+  StrikethroughIcon,
+  UnderlineIcon,
+} from "lucide-react";
 import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
 import { Underline } from "@tiptap/extension-underline";
@@ -9,6 +15,8 @@ import { Link } from "@tiptap/extension-link";
 import EditorLink from "./editor-components/link";
 import Heading from "@tiptap/extension-heading";
 import Headers from "./editor-components/headers";
+import Strike from "@tiptap/extension-strike";
+import Code from "@tiptap/extension-code";
 
 function MenuBar() {
   const { editor } = useCurrentEditor();
@@ -18,7 +26,7 @@ function MenuBar() {
   }
 
   return (
-    <div className="mb-4 flex gap-2">
+    <div className="mb-4 flex flex-wrap justify-start gap-2">
       <Headers />
       <Button
         variant="outline"
@@ -62,7 +70,30 @@ function MenuBar() {
         <UnderlineIcon className="h-6 w-6" />
       </Button>
 
+      <Button
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+        variant="outline"
+        size="icon"
+        className={editor.isActive("strike") ? "is-active" : ""}
+      >
+        <StrikethroughIcon className="h-6 w-6" />
+      </Button>
+
       <EditorLink />
+
+      <Button
+        variant="outline"
+        size="icon"
+        className={cn(
+          "flex gap-2",
+          editor.isActive("code")
+            ? "bg-slate-200 text-black dark:bg-white dark:text-black"
+            : "",
+        )}
+        onClick={() => editor.chain().focus().toggleCode().run()}
+      >
+        <CodeIcon className="h-6 w-6" />
+      </Button>
     </div>
   );
 }
@@ -79,10 +110,12 @@ export default function RichTextEditor({ content }: { content: string }) {
     Heading.configure({
       levels: [2, 3, 4, 5, 6],
     }),
+    Strike,
+    Code,
   ];
 
   return (
-    <div className="w-full rounded-lg border p-8 drop-shadow-md">
+    <div className="w-full rounded-lg border p-8 ">
       <EditorProvider
         slotBefore={<MenuBar />}
         extensions={extensions}
